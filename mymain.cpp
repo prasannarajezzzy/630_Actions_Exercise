08f721bd1a2c9b24d3637292dfe43a3770124b42
V.Prasanna <30752161+prasannarajezzzy@users.noreply.github.com>
2023-02-16 22:00:47 -0800

    added main.cpp

---
+++ mymain.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 5;
+
+    cout << "hello world" << endl;
+    cout << "The sum is " << a + b << endl;
+    return 0;
+}
